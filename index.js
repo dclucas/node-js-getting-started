d26ae7d70c2c443a45bea6d56db0d45a02078f82
Diogo Lucas <diogo.lucas@gmail.com>
2015-05-05 14:18:34 +0000

    Hello world now shows server IP and port

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var app = express();
+var osIP = undefined
+
+require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+  osIP = add
+})
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var port = server.address().port;
+  response.send('Hello World from ' + osIP + ':' + port + '!');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+var server = app.listen(app.get('port'), function() {
+  //console.log('Node app is running on port', app.get('port'));
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
 });
